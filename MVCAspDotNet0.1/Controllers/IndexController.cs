4f6168679903abb1495a7764ef9292bcaa0c5a05
rajdharak <raj@promact.com>
2016-07-04 13:11:18 +0530

    new Project01

---
+++ MVCAspDotNet0.1/Controllers/IndexController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAspDotNet0._1.Controllers
+{
+    public class IndexController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            ViewBag.Countries = new List<string>()
+            {
+                "India",
+                "usa",
+                "pakistan"
+        };
+            ViewData["Countries"] = new List<string>()
+            {
+                "India",
+                "usa",
+                "pakistan"
+        };
+            return View();
+        }
+        
+    }
+}
